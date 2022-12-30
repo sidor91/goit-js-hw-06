835b8ff0d0e1de8a8e7c3d92bc55ac0937f8a659
Serhii Sydorenko <sidorchik91@gmail.com>
2022-12-30 14:11:30 +0200

    task 4 complete

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+const counterElement = document.querySelector('#value');
+let counterValue = 0;
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+
+incrementBtn.addEventListener('click', onIncrementBtn);
+decrementBtn.addEventListener('click', onDecrementBtn);
+
+function onIncrementBtn() {
+    counterValue += 1;
+    counterElement.textContent = `${counterValue}`;
+}
+
+function onDecrementBtn() {
+    counterValue -= 1;
+    counterElement.textContent = `${counterValue}`;
+}
+
+console.log(counterElement.textContent);
