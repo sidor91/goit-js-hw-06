4e3c34c56504afc73d7cd9dd76c0e0ebd15b33e2
Serhii Sydorenko <sidorchik91@gmail.com>
2022-12-30 16:59:57 +0200

    task 8 complete

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
 
+const loginFrom = document.querySelector('.login-form');
+
+loginFrom.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all the fields!");
+    }
+
+    const result = { Email: `${email.value}`, Password: `${password.value}` };
+    console.log(result);
+  event.currentTarget.reset();
+};
