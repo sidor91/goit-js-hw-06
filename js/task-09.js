691dbd44d616ba653cc3254248f4f6cf30dc7509
Serhii Sydorenko <sidorchik91@gmail.com>
2022-12-30 17:52:10 +0200

    task 9 complete

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const colorChangeButton = document.querySelector('.change-color');
+const bodySection = document.querySelector('body');
+const colorIdentificator = document.querySelector('.color');
+
+colorChangeButton.addEventListener('click', changeBodyColor);
+
+function changeBodyColor(event) {
+  bodySection.style.backgroundColor = getRandomHexColor();
+  colorIdentificator.textContent = `${bodySection.style.backgroundColor}`;
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
