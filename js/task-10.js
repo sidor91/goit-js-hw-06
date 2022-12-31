5caced48d8f3241c0c484533aad29daf2c7fc6d8
Serhii Sydorenko <sidorchik91@gmail.com>
2022-12-31 12:00:04 +0200

    task 10 complete

--- js/task-10.js
+++ js/task-10.js
@@ -1,31 +1,11 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 // -------------------- divBoxes ----------------
 const boxWithBoxes = document.querySelector('#boxes');
-
-// -------------------- Create --------------------
-const createButton = document.querySelector('[data-create]');
-createButton.addEventListener('click', onCreateButtonClick);
-
-function onCreateButtonClick(event) {
-  createBoxes(numberOfBoxes);
-}
-
-function createBoxes(numberOfBoxes) {
-  for (let i = 1; i <= numberOfBoxes; i += 1) {
-    const boxElem = document.createElement('div');
-    boxElem.style.backgroundColor = `${getRandomHexColor()}`;
-    boxElem.style.width = `(10 * ${i} + 30)`;
-    boxElem.style.height = `(10 * ${i} + 30)`;
-    boxWithBoxes.append(boxElem);
-  }
-}
-
-// -------------------- Destroy --------------------
-const destroyButton = document.querySelector('[data-destroy]');
-// destroyButton.addEventListener('click', onDestroyButtonClick);
 
 // -------------------- Input --------------------
 
@@ -35,6 +15,43 @@ let numberOfBoxes;
 inputField.addEventListener('input', onInputAction);
 
 function onInputAction(event) {
-  numberOfBoxes = Number(event.currentTarget.value);
+   numberOfBoxes = Number(event.currentTarget.value);
 }
+
+// -------------------- Create --------------------
+const createButton = document.querySelector('[data-create]');
+createButton.addEventListener('click', onCreateButtonClick);
+
+function onCreateButtonClick(event) {
+  if (numberOfBoxes >= 1 && numberOfBoxes <= 100) {
+    createBoxes(numberOfBoxes);
+    return;
+  }
+  alert('the value should be from 1 to 100!');
+  onDestroyButtonClick();
+}
+
+ let boxesArr = [];
+
+function createBoxes(numberOfBoxes) {
+  for (let i = 1; i <= numberOfBoxes; i += 1) {
+    const boxElem = document.createElement('div');
+    boxElem.style.backgroundColor = `${getRandomHexColor()}`;
+    const boxSizeValue = (i * 10 + 20);
+    boxElem.style.width = `${boxSizeValue}px`;
+    boxElem.style.height = `${boxSizeValue}px`;
+    boxesArr.push(boxElem);
+  }
+    boxWithBoxes.append(...boxesArr);
+}
+
+// -------------------- Destroy --------------------
+const destroyButton = document.querySelector('[data-destroy]');
+destroyButton.addEventListener('click', onDestroyButtonClick);
+
+function onDestroyButtonClick() {
+  boxWithBoxes.innerHTML = '';
+  inputField.value = 0;
+  boxesArr = [];
+};
 
